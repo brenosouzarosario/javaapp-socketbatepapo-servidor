56b52bdbf39936e3fbe848d84bf9329fe95994c1
Breno Souza <brenosouza.rosario@gmail.com>
2020-07-11 18:45:57 -0300

    first commit

---
+++ src/Model/Servidor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Controller.ServidorController;
+
+/**
+ *
+ * @author Breno
+ */
+public class Servidor {
+    
+        public static void main(String[] args) {
+        new ServidorController();
+    }
+    
+}
